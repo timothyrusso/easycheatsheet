72867d3d90cf3afbe907864af5a4492fb316e06b
timothyrusso <russotimothy@live.it>
2021-11-15 08:07:34 +0100

    feat: block section and styling

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const blocksList = document.querySelector('.blocks-grid'); // Let's find the pla
 
 function generateBlock(block) { //---GENERATE CARDS
     const blockElement = blockTemplate.cloneNode(true); // Clone template card
-    const blockDeleteButton = blockElement.querySelector('.block__delete'); // Query delete button
+    const blockEditButton = blockElement.querySelector('.block__edit'); // Query delete button
     const blockTitle = blockElement.querySelector('.block__title'); // Query image caption element
     const importanceButton = blockElement.querySelector('.block__important'); // Query the like button
-    blockElement.querySelector('.block__title').textContent = block.title; // Query title element
+    const blockDescription = blockElement.querySelector('.block__description');
+    const blockExample = blockElement.querySelector('.block__example');
+    const blockLink = blockElement.querySelector('.block__link');
     blockTitle.textContent = block.title;
+    blockDescription.textContent = block.description;
+    blockExample.textContent = block.example;
+    blockLink.href = block.link;
     importanceButton.addEventListener('click', function (evt) {
       evt.target.classList.toggle('block__importance_active');
     });
-    blockDeleteButton.addEventListener('click', () => onDeleteClick(blockElement));
+    blockEditButton.addEventListener('click', () => onDeleteClick(blockElement));
     return blockElement;
   }
   
--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 const initialBlocks = [
     {
-      title: "Declare a variable",
-      description: "var = ciaoPippo",
-      example: "var = ciaoPippo;",
-      link: "./images/palouse-falls.png",
+      title: "forEach()",
+      description: "Executes a provided function once for each array element.",
+      example: "numbers.forEach(number => console.log(number));",
+      link: "https://www.freecodecamp.org/news/javascript-foreach-how-to-loop-through-an-array-in-js/",
       language: "Javascript"
     }
   ];
