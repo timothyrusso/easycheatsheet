98ce2656138b170ee1d00f39837687e367aff117
timothyrusso <russotimothy@live.it>
2021-11-15 08:25:09 +0100

    feat: new content in the blocks

--- data.js
+++ data.js
@@ -1,9 +1,16 @@
 const initialBlocks = [
-    {
-      title: "forEach()",
-      description: "Executes a provided function once for each array element.",
-      example: "numbers.forEach(number => console.log(number));",
-      link: "https://www.freecodecamp.org/news/javascript-foreach-how-to-loop-through-an-array-in-js/",
-      language: "Javascript"
-    }
-  ];
+  {
+    title: "forEach()",
+    description: "Executes a provided function once for each array element.",
+    example: "numbers.forEach(number => console.log(number));",
+    link: "https://www.freecodecamp.org/news/javascript-foreach-how-to-loop-through-an-array-in-js/",
+    language: "Javascript"
+  },
+  {
+    title: "map()",
+    description: "Allows you to iterate over an array and modify its elements using a callback function. The callback function will then be executed on each of the array's elements.",
+    example: "arr.map(function(element, index, array){  }, this);",
+    link: "https://www.freecodecamp.org/news/javascript-map-how-to-use-the-js-map-function-array-method/",
+    language: "Javascript"
+  }
+];
